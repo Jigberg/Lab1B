ff582a0ae1ed2becaf6fa31dc1ff633ef8d608d8
Fredins <fredin.martin@gmail.com>
2020-11-18 17:46:42 +0100

    Revert "Add files via upload"

--- DIT953-lab1-master/DIT953-lab1-master/src/Carriers/CarTransportCarrier.java
+++
@@ -1,9 +0,0 @@
-package Carriers;
-import java.util.ArrayList;
-
-public class CarTransportCarrier extends Carrier<Vehicle>{
-    public CarTransportCarrier(){
-        super(6, new ArrayList<>());
-
-    }
-}
--- DIT953-lab1-master/DIT953-lab1-master/src/Carriers/FerryCarrier.java
+++
@@ -1,9 +0,0 @@
-package Carriers;
-import java.util.ArrayList;
-import Vehicles.Vehicle;
-
-public class FerryCarrier extends Carrier<Vehicle>{
-    public FerryCarrier(){
-        super(20, new ArrayList<>());
-    }
-}
