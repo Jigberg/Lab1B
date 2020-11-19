d3637c3e04951b7afb868268797870eeff759d66
Luke-png <72192057+Luke-png@users.noreply.github.com>
2020-11-19 14:38:28 +0100

    Add files via upload

--- DIT953-lab1-master/src/Vehicles/Ferry.java
+++ DIT953-lab1-master/src/Vehicles/Ferry.java
@@ -2,15 +2,8 @@ package Vehicles;
 
 import Carriers.Carrier;
 import Carriers.FerryCarrier;
-import Carriers.IFerryCarrier;
-import Positions.Direction;
-
-import java.awt.*;
 
 public class Ferry extends Vehicle {
-    private Carrier<IFerryCarrier, Ferry> carrier = new FerryCarrier(this);
+    private Carrier<Vehicle> carrier = new FerryCarrier();
 
-    public Ferry(double x, double y, Direction direction, double currentSpeed, boolean isMovable, double enginePower, Color color, String modelName) {
-        super(x, y, direction, currentSpeed, isMovable, enginePower, color, modelName);
-    }
 }
--- DIT953-lab1-master/src/Vehicles/Saab95.java
+++ DIT953-lab1-master/src/Vehicles/Saab95.java
@@ -1,38 +1,62 @@
 package Vehicles;
 
-import Carriers.ICarTransportCarrier;
-import Carriers.IFerryCarrier;
-import Positions.Direction;
 import java.awt.*;
 
 /**
  * @author Nicklas, Lukas, Emil, Martin.
  * A class for car model Vehicles.Saab95
  */
-public class Saab95 extends Vehicle implements ICarTransportCarrier, IFerryCarrier {
+public class Saab95 extends Vehicle {
     private boolean turboOn;
-    private double turboValue;
 
     /**
      * Constructor for class.
      * Turns turbo off.
      */
-    public Saab95(double x, double y, Direction direction, double currentSpeed, boolean isMovable, Color color, boolean turboOn, double turboValue) {
-        super(x, y, direction, currentSpeed, isMovable, 150, color, "Saab95");
-        this.turboOn = turboOn;
-        this.turboValue = turboValue;
+    public Saab95() {
+        super(125, Color.red, "Vehicles.Saab95");
+        turboOn = false;
     }
     /**
      * Turns the turbo on.
      */
-    void setTurboOn(boolean turboOn) { this.turboOn = turboOn; }
+    void setTurboOn() {
+        turboOn = true;
+    }
+    /**
+     * Turns the turbo off.
+     */
+    void setTurboOff() {
+        turboOn = false;
+    }
+
     /**
      * Calculates the speedFactor.
      * Uses turbo.
      */
     @Override
-    public double speedFactor() { return getEnginePower() * 0.01 * (getTurboOn() ? getTurboValue() : 1); }
-    public boolean getTurboOn(){ return this.turboOn; }
-    public double getTurboValue(){ return this.turboValue; }
+    double speedFactor() {
+        double turbo = 1;
+        if (turboOn) turbo = 1.3;
+        return getEnginePower() * 0.01 * turbo;
+    }
+
+    /**
+     * Increments the cars speed.
+     * @param amount to multiply the speedFactor with.
+     */
+    @Override
+    void incrementSpeed(double amount) {
+        setCurrentSpeed(getCurrentSpeed() + speedFactor() * amount);
+    }
+
+    /**
+     * Decrements the speed.
+     * @param amount to multiply the speedfactor with.
+     */
+    @Override
+    void decrementSpeed(double amount) {
+        setCurrentSpeed(getCurrentSpeed() - speedFactor() * amount);
+    }
 
 }
